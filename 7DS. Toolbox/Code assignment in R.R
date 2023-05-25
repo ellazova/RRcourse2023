1931da57b2724e3aa2e5b5766758f07318080f0f
ellazova <l.ellazova@student.uw.edu.pl>
2023-05-25 17:57:12 +0200

    some new commit

--- 7DS. Toolbox/Code assignment in R.R
+++ 7DS. Toolbox/Code assignment in R.R
@@ -1,6 +1,5 @@
-
 # Sets the path to the parent directory of RR classes
-setwd("Z:\\File folders\\Teaching\\Reproducible Research\\2023\\Repository\\RRcourse2023\\6. Coding and documentation")
+setwd("C:\\Users\\ellaz\\OneDrive\\Desktop\\RRcourse\\RRcourse2023\\6. Coding and documentation")
 
 #   Import data from the O*NET database, at ISCO-08 occupation level.
 # The original data uses a version of SOC classification, but the data we load here
